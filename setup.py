27268b783d4d0f0b6868c6e77b2f0af6ae6fc74d
mitchfriedman <mitchfriedman5@gmail.com>
2015-09-29 20:25:39 -0400

    Remove entry point

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Siphon-Client',
+    name='siphon',
     version='0.0.1',
     author='mitch',
     author_email='mitchfriedman@gmail.com',
@@ -10,11 +10,6 @@ setup(
     include_package_data=True,
     install_requires=[
         'clint', 'flake8',
-    ],
-    entry_points={
-        'console_scripts': [
-            'sc = client.client.run',
-        ]
-    }
+    ]
 )
 
